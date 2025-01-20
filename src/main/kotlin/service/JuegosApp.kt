0b2130925b65cefb30cb191e020b138dc248dd32
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2025-01-20 11:02:50 +0100

    Acabado todo pero faltarian preguntas y preguntar sobre cerrar la conexión (Transacción no funcional !!)

--- src/main/kotlin/service/JuegosApp.kt
+++ src/main/kotlin/service/JuegosApp.kt
@@ -5,27 +5,14 @@ import model.Juego
 
 class JuegosApp {
 
-    private val db = ConexionBD.getDatabase("fbatlos")
-    private val collection = db.getCollection("coll_juegos")
 
     fun listarJuegos(): List<Juego> {
-        val listado =  collection.find().map { doc ->
-            Juego(
-                titulo = doc.getString("titulo"),
-                genero = doc.getString("genero"),
-                precio = doc.getDouble("precio"),
-                fecha_lanzamiento = doc.getDate("fecha_lanzamiento")
-            )
-        }.toList()
 
-        return listado
-    }
+       return try {
+            val db = ConexionBD.getDatabase("fbatlos")
+            val collection = db.getCollection("coll_juegos")
 
-
-    fun buscarPorGenero(genero: String): List<Juego> {
-        val buscarGenero = collection.find(org.bson.Document("genero", genero))
-            .sort(org.bson.Document("titulo", 1))
-            .map { doc ->
+            val listado =  collection.find().map { doc ->
                 Juego(
                     titulo = doc.getString("titulo"),
                     genero = doc.getString("genero"),
@@ -34,11 +21,46 @@ class JuegosApp {
                 )
             }.toList()
 
-        if (buscarGenero.isEmpty()){
-            println("No se ha encontrado ese genero")
-        }
+            return listado
 
-        return buscarGenero
+        }catch (e : Exception){
+            println(e.message)
+           return listOf()
+        }finally {
+            ConexionBD.close()
+        }
+    }
+
+
+    fun buscarPorGenero(genero: String): List<Juego> {
+
+
+        return try {
+            val db = ConexionBD.getDatabase("fbatlos")
+            val collection = db.getCollection("coll_juegos")
+
+            val buscarGenero = collection.find(org.bson.Document("genero", genero))
+                .sort(org.bson.Document("titulo", 1))
+                .map { doc ->
+                    Juego(
+                        titulo = doc.getString("titulo"),
+                        genero = doc.getString("genero"),
+                        precio = doc.getDouble("precio"),
+                        fecha_lanzamiento = doc.getDate("fecha_lanzamiento")
+                    )
+                }.toList()
+
+            if (buscarGenero.isEmpty()){
+                println("No se ha encontrado ese genero")
+            }
+            return buscarGenero
+
+        }catch (e : Exception){
+            println(e.message)
+            return listOf()
+        }finally {
+            ConexionBD.close()
+        }
     }
 
 
@@ -47,50 +69,84 @@ class JuegosApp {
             return "Error: Falta algun campo del juego."
         }
 
-        val existe = collection.find(org.bson.Document("titulo", juego.titulo)).first() != null
+        return try {
+            val db = ConexionBD.getDatabase("fbatlos")
+            val collection = db.getCollection("coll_juegos")
 
-        if (existe) {
+            val existe = collection.find(org.bson.Document("titulo", juego.titulo)).first() != null
 
-            return ("Error: Ya existe un juego con el titulo '${juego.titulo}'")
+            if (existe) {
 
-            /* No se puede hacer ciclica porque hace la inyeccion de todas la veces fallada
-            println("Dame otro titulo: ")
-            juego.titulo = readLine() ?: ""
+                return ("Error: Ya existe un juego con el titulo '${juego.titulo}'")
 
-            registrarJuego(juego)
-             */
+                /* No se puede hacer ciclica porque hace la inyeccion de todas la veces fallada
+                println("Dame otro titulo: ")
+                juego.titulo = readLine() ?: ""
+
+                registrarJuego(juego)
+                 */
+            }
+
+            collection.insertOne(
+                org.bson.Document()
+                    .append("titulo", juego.titulo)
+                    .append("genero", juego.genero)
+                    .append("precio", juego.precio)
+                    .append("fecha_lanzamiento", juego.fecha_lanzamiento)
+            )
+            return "Juego registrado exitosamente."
+
+        }catch (e : Exception){
+            println(e.message)
+            return "Error: No se ha encontrado ese genero"
+        }finally {
+            ConexionBD.close()
         }
-
-        collection.insertOne(
-            org.bson.Document()
-                .append("titulo", juego.titulo)
-                .append("genero", juego.genero)
-                .append("precio", juego.precio)
-                .append("fecha_lanzamiento", juego.fecha_lanzamiento)
-        )
-        return "Juego registrado exitosamente."
     }
 
 
     fun eliminarPorGenero(genero: String): String {
-        val result = collection.deleteMany(org.bson.Document("genero", genero))
-        return "Se han eliminado ${result.deletedCount} juegos del genero '$genero'."
+        return try {
+            val db = ConexionBD.getDatabase("fbatlos")
+            val collection = db.getCollection("coll_juegos")
+
+            val result = collection.deleteMany(org.bson.Document("genero", genero))
+            return "Se han eliminado ${result.deletedCount} juegos del genero '$genero'."
+
+        }catch (e:Exception){
+            println(e.message)
+            return "Error: No se ha encontrado ese genero"
+        }finally {
+            ConexionBD.close()
+        }
     }
 
 
     fun modificarJuego(titulo: String, nuevosDatos: Juego): String {
-        val resultado = collection.updateOne(
-            org.bson.Document("titulo", titulo),
-            org.bson.Document("\$set", org.bson.Document()
-                .append("genero", nuevosDatos.genero)
-                .append("precio", nuevosDatos.precio)
-                .append("fecha_lanzamiento", nuevosDatos.fecha_lanzamiento)
+        return try {
+            val db = ConexionBD.getDatabase("fbatlos")
+            val collection = db.getCollection("coll_juegos")
+
+            val resultado = collection.updateOne(
+                org.bson.Document("titulo", titulo),
+                org.bson.Document(
+                    "\$set", org.bson.Document()
+                        .append("genero", nuevosDatos.genero)
+                        .append("precio", nuevosDatos.precio)
+                        .append("fecha_lanzamiento", nuevosDatos.fecha_lanzamiento)
+                )
             )
-        )
-        return if (resultado.matchedCount > 0) {
-            "Juego actualizado exitosamente."
-        } else {
-            "No se encontro un juego con el titulo '$titulo'."
+            return if (resultado.matchedCount > 0) {
+                "Juego actualizado exitosamente."
+            } else {
+                "No se encontro un juego con el titulo '$titulo'."
+            }
+
+        }catch (e:Exception){
+            println(e.message)
+            return "Error: No se ha encontrado ese genero"
+        }finally {
+            ConexionBD.close()
         }
     }
 }
